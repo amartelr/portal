849fecdbdabdce3ddcf4d052c73128ea3641c9f8
Alfredo Martel <alfredo.martel@gmail.com>
2018-03-02 16:18:53 +0000

    Adding First Configuration from json file

--- Readme.md
+++ Readme.md
@@ -1,2 +1,10 @@
-# First Run
+# Steps
+
+## First Run
+
 go run portal.go
+
+## Second Run
+
+go build portal.go && ./portal
+go build portal.go -o portal.run
--- portal.go
+++ portal.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"encoding/json"
+
+	"log"
+	"os"
+
 	"github.com/amartelr/portal/webportal"
 )
 
+type configuration struct {
+	ServerAddress string `json:"webserver"`
+}
+
 func main() {
-	webportal.Run("localhost:8000")
+	file, err := os.Open("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config := new(configuration)
+	json.NewDecoder(file).Decode(config)
+
+	log.Println("Connecting ... on addr ", config.ServerAddress)
+	webportal.Run(config.ServerAddress)
 }
